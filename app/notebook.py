0fdf3bcb078794dafba404335c281e1564132d30
ValeriaMonsalve <valeriaalvarez448@gmail.com>
2025-02-18 13:31:01 -0500

    metodo inicializacion

--- app/notebook.py
+++ app/notebook.py
@@ -9,6 +9,13 @@ class Nota:
 
 
 
+  def __int__(self, code: str, title: str, text: str, importance: str):
+        self.code: str = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date = datetime.now()
+        self.tags: list[str] = []
 
 
 
